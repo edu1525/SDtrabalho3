cc7c3b47c3503602c1dbad58c86c24b3fe802044
Lucas Bernardes <lucascbernardes@live.com>
2016-11-19 15:14:40 -0200

    Como faz

--- projeto3/HTTPServer.java
+++ projeto3/HTTPServer.java
@@ -10,80 +10,41 @@ import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.TException;
 
-
-
-public class HTTPServer implements Server.Iface{
+public class HTTPServer implements Server.Iface {
     private int serverName;
-    private HTTPServer nextServer, previousServer;
-
     private int port;
+    private HTTPServer nextServer, previousServer;
     private ServerSocket socket;
     private FileTree ft = new FileTree();
     private boolean isOnline = true;
     private Server.Processor processor;
 
-
     public HTTPServer(int port, int serverName) throws IOException {
-
         processor = new Server.Processor(this);
 
         this.port = port;
         this.serverName = serverName;
 
-        /* Cria a arvore de arquivos
-        ft.addFile("/a", "aaaaaaaaa");
-        ft.addFile("/b", "bbbbbbbbbb");
-        ft.addFile("/a/c", "cccccc");
-        ft.addFile("/b/d", "ddddddd");
-        //*/
-
-        /*
-        try {
-            // Cria um socket para o servidor na porta recebida
-            this.socket = new ServerSocket(port);
-            System.err.println("Started server on port " + port);
-        }
-        catch (IOException e) {
-            System.err.println("Could not create server on port " + port);
-            System.exit(-1);
-        }
-        */
-       // while (this.isOnline) {
-            // Cria um socket para o cliente e espera uma requisicao
-            //System.err.println("Waiting for connection...");
-            //Socket client = this.socket.accept();
-            //System.err.println("Accepted connection from client " +
-            //client.getInetAddress().toString());
-            // Dispara uma thread para atender a requisicao e aguarda uma proxima
-            //new Thread(new Request(this, client)).start();
-
-            Runnable run = new Runnable(){
-                public void run(){
-                    simple(processor, port);
-
-                } 
-
-            };
-
-            
-        //}
-        new Thread (run).start();
-
-        //System.err.println("Server halting...");
-        //this.socket.close();
+        Runnable run = new Runnable() {
+            public void run() {
+                simple(processor, port);
+            }
+        };
+        new Thread(run).start();
     }
 
-     public void simple(Server.Processor processor, int port) {
+    public void simple(Server.Processor processor, int port) {
         try {
-          TServerTransport serverTransport = new TServerSocket(port);
-          TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
-
-          System.out.println("Starting server "+serverName+"...");
-          server.serve();
+            TServerTransport serverTransport = new TServerSocket(port);
+            TServer server = new TSimpleServer(
+                new Args(serverTransport).processor(processor)
+            );
+            System.out.println("Starting server " + serverName + "...");
+            server.serve();
         } catch (Exception e) {
-          e.printStackTrace();
+            e.printStackTrace();
         }
-  }
+    }
 
     public File getFile(String filepath) {
         return this.ft.getFile(filepath);
@@ -114,116 +75,100 @@ public class HTTPServer implements Server.Iface{
     public String GET(String path) throws org.apache.thrift.TException{
         File f = getFile(path);
         if (f != null) {
-            String header = "HTTP/1.1 200 OK\n" + "Verion: " + f.getVersion() + "\nCreation: " 
+            String header = "HTTP/1.1 200 OK\n" + "Verion: " + f.getVersion() + "\nCreation: "
             + f.getCreationTime() + "\nModification: " +
                     f.getModificationTime() + "\nContent-length: " +
                         f.getData().length() + '\n';
 
-            String retorno = header + f.getData()+"\n";        
+            String retorno = header + f.getData()+"\n";
             return retorno;
         }
         return null;
-        
     }
 
-
-
-    public String LIST(String path) throws org.apache.thrift.TException{
+    public String LIST(String path) throws org.apache.thrift.TException {
         File file = getFile(path);
-
         ArrayList<File> child = file.getChildren();
 
-        if(child != null){
-
+        if (child != null) {
             StringBuilder builder = new StringBuilder();
 
-            for(File f : child){
+            for (File f : child) {
                 builder.append(f.getName()+"\n");
-            }        
-           
+            }
             return builder.toString();
-        } else{
-
+        } else {
             return null;
         }
-
     }
 
-    public boolean ADD(String path, String data) throws org.apache.thrift.TException{
+    public boolean ADD(String path, String data) throws
+    org.apache.thrift.TException {
         if (addFile(path, data) == null) return false;
         else return true;
-
-
     }
 
-    public boolean UPDATE(String path, String data) throws org.apache.thrift.TException{
+    public boolean UPDATE(String path, String data) throws
+    org.apache.thrift.TException {
         File f = getFile(path);
 
         if (f != null) {
             f.addData(data);
             return true;
-
-        } else return false;
+        } else
+            return false;
     }
 
-    public boolean DELETE(String path) throws org.apache.thrift.TException{
+    public boolean DELETE(String path) throws org.apache.thrift.TException {
         return removeFile(path);
-
-
     }
 
-    public boolean UPDATE_VERSION(String path, String data, int version) throws org.apache.thrift.TException{
+    public boolean UPDATE_VERSION(String path, String data, int version) throws         org.apache.thrift.TException {
         File f = getFile(path);
 
-        if(f != null && f.getVersion() == version){
+        if (f != null && f.getVersion() == version) {
             f.addData(data);
             return true;
-
-        } else return false;
-
-
-
+        } else
+            return false;
     }
 
-    public boolean DELETE_VERSION(String path, int version) throws org.apache.thrift.TException{
-
+    public boolean DELETE_VERSION(String path, int version) throws
+    org.apache.thrift.TException {
         File f = getFile(path);
 
-        if(f != null && f.getVersion() == version){
+        if (f != null && f.getVersion() == version) {
             return removeFile(path);
-
-        } else return false;
-
+        } else
+            return false;
     }
 
-  
-
-    public int getServerName(){
+    public int getServerName() {
         return this.serverName;
     }
 
-    public void setNextServer(HTTPServer s){
+    public void setNextServer(HTTPServer s) {
         this.nextServer = s;
     }
 
-    public void setPreviousServer(HTTPServer s){
+    public void setPreviousServer(HTTPServer s) {
         this.previousServer = s;
     }
 
-    public HTTPServer getNextServer(){
+    public HTTPServer getNextServer() {
         return this.nextServer;
     }
 
-    public HTTPServer getPreviousServer(){
+    public HTTPServer getPreviousServer() {
         return this.previousServer;
-
     }
 
-    public boolean equals(Object o ){
+    public boolean equals(Object o) {
         HTTPServer server = (HTTPServer) o;
 
-        if((server != null) && (server.getServerName() == this.serverName)) return true;
-        else return false;
-
+        if ((server != null) && (server.getServerName() == this.serverName))
+            return true;
+        else
+            return false;
     }
 }
--- projeto3/Servers.java
+++ projeto3/Servers.java
@@ -1,16 +1,11 @@
-
-
 import java.util.ArrayList;
 import java.io.IOException;
 import org.apache.thrift.TException;
 
-
-public class Servers{
+public class Servers {
 	private static ArrayList<HTTPServer> servers;
 
-
 	public static void main(String[] args) throws IOException {
-
 		servers = new ArrayList<HTTPServer>();
 
 		HTTPServer s1 = new HTTPServer(7000, 0);
@@ -21,132 +16,96 @@ public class Servers{
 		servers.add(s2);
 		servers.add(s3);
 
-		try{
-
-			ADD("/user","eduardo");
-			ADD("/user/age","21 anos");
-			System.out.println(GET("/user")+" - \n"+GET("/user/age"));
-		} catch(TException e){
+		try {
+			ADD("/user", "eduardo");
+			ADD("/user/age", "21 anos");
+			System.out.println(GET("/user") + " - \n" +GET("/user/age"));
+		} catch (TException e) {
 			e.printStackTrace();
 		}
-
-
-
-
 	}
 
-
-	public static void setServer(int port, int serverName){
-
-		try{
+	public static void setServer(int port, int serverName) {
+		try {
 			HTTPServer server = new HTTPServer(port, serverName);
 			servers.add(server);
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 
-	public static HTTPServer getServer(int serverName){
+	public static HTTPServer getServer(int serverName) {
 		return servers.get(serverName);
 	}
 
-	public static String GET(String path) throws org.apache.thrift.TException{
+	public static String GET(String path)
+	throws org.apache.thrift.TException {
 		int hash = path.hashCode() % servers.size();
 
-		for(HTTPServer server : servers){
-
+		for (HTTPServer server : servers) {
 			String r = server.GET(path);
 
-			if(r != null){
-				System.out.println("Dado encontrado no servidor "+server.getServerName());
-				return r; 
-			} 
-
-
+			if (r != null) {
+				System.out.println("Dado encontrado no servidor " +
+					server.getServerName()
+				);
+				return r;
+			}
 		}
 		return null;
-
 	}
 
-    public static String LIST(String path) throws org.apache.thrift.TException{
-    	for(HTTPServer server : servers){
-
+    public static String LIST(String path) throws org.apache.thrift.TException {
+    	for (HTTPServer server : servers) {
     		String r = server.LIST(path);
-			if( r != null) return r;
-
-
+			if (r != null)
+				return r;
 		}
 		return null;
-
-
-
     }
 
-    public static boolean ADD(String path, String data) throws org.apache.thrift.TException{
+    public static boolean ADD(String path, String data)
+	throws org.apache.thrift.TException {
     	int hash = (path.hashCode() % servers.size());
-
     	HTTPServer server = getServer(hash);
 
-    	System.out.println("Dado inserido no servidor "+server.getServerName());
+    	System.out.println("Dado inserido no servidor " + server.getServerName());
 
     	return server.ADD(path, data);
-
-
-
     }
 
-    public static boolean UPDATE(String path, String data) throws org.apache.thrift.TException{
-    	for(HTTPServer server : servers){
-
+    public static boolean UPDATE(String path, String data)
+	throws org.apache.thrift.TException {
+    	for (HTTPServer server : servers) {
     		boolean r = server.UPDATE(path, data);
-			if(r) return true;
-
-
+			if (r) return true;
 		}
-
 		return false;
-
     }
 
-    public static boolean DELETE(String path) throws org.apache.thrift.TException{
-    	for(HTTPServer server : servers){
-
+    public static boolean DELETE(String path) throws org.apache.thrift.TException {
+    	for (HTTPServer server : servers){
 			boolean r = server.DELETE(path);
-			if(r) return true;
-
-
+			if (r) return true;
 		}
-
 		return false;
-
-
     }
 
     public static boolean UPDATE_VERSION(String path, String data, int version) throws org.apache.thrift.TException{
-    	for(HTTPServer server : servers){
-
+    	for (HTTPServer server : servers){
 			boolean r = server.UPDATE_VERSION(path, data, version);
-			if(r) return true;
-
-
+			if (r) return true;
 		}
-
 		return false;
     }
 
-    public static boolean DELETE_VERSION(String path, int version) throws org.apache.thrift.TException{
-    	for(HTTPServer server : servers){
-
+    public static boolean DELETE_VERSION(String path, int version) throws
+	org.apache.thrift.TException {
+    	for (HTTPServer server : servers) {
 			boolean r = server.DELETE_VERSION(path, version);
-			if(r) return true;
-
-
+			if (r) return true;
 		}
-
 		return false;
-
     }
-
 
 }
